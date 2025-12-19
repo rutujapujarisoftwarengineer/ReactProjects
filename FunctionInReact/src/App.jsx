e76b620161a8eee80c5f8c4aa6eb56d61ae94298
Rutuja Pujari <rutujapujari13@gmail.com>
2025-12-19 13:09:46 +0530

    First commit

---
+++ FunctionInReact/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+// function App() {
+//   function callFun() {
+//     alert("Function called");
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={callFun}>Click Me</button>
+//     </div>
+//   );
+// }
+
+//Using Arrow function
+// function App() {
+//   const fruits = () => {
+//     alert("Apple");
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={fruits}>Click Me</button>
+//     </div>
+//   );
+// }
+
+//Passing Parameters to Functions in JSX
+// function App() {
+//   const fruits = (name) => {
+//     alert(name);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={() => fruits("Apple")}>Apple</button>
+//       <button onClick={() => fruits("Banana")}>Banana</button>
+//     </div>
+//   );
+// }
+export default App;
